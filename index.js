cf7d4e0a163db67d45bc0deb4a0539e0c5cebc35
avner mindelis <avnerm@checkpoint.com>
2023-08-30 17:41:14 +0300

    added a download button

--- index.js
+++ index.js
@@ -92,8 +92,33 @@ function handleSubmit() {
             "BasicEnrichmentConfig": BasicEnrichmentConfig,
         }
     };
-
     document.getElementById("result").innerText = JSON.stringify(json, replacer, 2);
+    return json;
+}
+
+function saveTextAsFile() {
+    var textToWrite = handleSubmit()
+    var textFileAsBlob = new Blob([textToWrite], {type: 'text/plain'});
+
+    var fileNameToSaveAs = textToWrite.Entity + ".json";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = () => {
+            document.body.removeChild(event.target);
+        };
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
 }
 
 function isEmpty(value) {
