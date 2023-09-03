c9ea16e6f1c2ee79992ea7e89e06e4a8ac9d9548
avner mindelis <avnerm@checkpoint.com>
2023-09-03 18:59:12 +0300

    implemented stash and pop

--- index.js
+++ index.js
@@ -53,16 +53,11 @@ function addRequestParametersAnswer() {
 function handleSubmit() {
     const data = new FormData(form);
     let obj = Object.fromEntries(data.entries());
-    const stripAsyncOrRequest = str => str.replace(/(async|request)$/i, '');
-    const stripAwsOrEntity = str => str.replace(/^(Aws)/i, '').replace(/(Entity)$/i, '');
-    const stripJson = str => str.replace(/(.json)$/i, '');
 
-    if (document.getElementById("PropertiesToRemoveFromExternalObject").disabled)
-    {
+    if (document.getElementById("PropertiesToRemoveFromExternalObject").disabled) {
         obj.PropertiesToRemoveFromExternalObject = ""
     }
-    if (document.getElementById("ENRPropertiesToRemoveFromExternalObject").disabled)
-    {
+    if (document.getElementById("ENRPropertiesToRemoveFromExternalObject").disabled) {
         obj.ENRPropertiesToRemoveFromExternalObject = ""
     }
 
@@ -70,28 +65,20 @@ function handleSubmit() {
 
     EntityName = "Aws" + obj.ruleTargetType
 
-    let ContinueWith = Array.from(document.querySelectorAll('#ContinueWithList li')).map(function (li) {
-        return stripJson(li.textContent);
-    });
+    let ContinueWith = getContinueWith()
 
-    let RequestParameters = Array.from(document.querySelectorAll('#RequestParametersList li')).map(function (li) {
-        let val = li.textContent.replace(/[\{\}"]/g, '').split(':')
-        return {
-            "ParameterName": val[0], "ParameterValue": val[1]
-        };
-    });
+    let RequestParameters = getRequestParameters()
 
     if (obj.MaxPages !== '') {
-        RequestParameters.push({"ParameterName": "MaxPages", "ParameterValue": obj.MaxPages})
+        RequestParameters.push({"ParameterName": "MaxPages", "ParameterValue": parseInt(obj.MaxPages)})
     }
 
-
     let BasicEnrichmentConfig = (document.getElementById("ShouldEnrichBaseEntity").checked) ? {
-        "ApiCall": stripAsyncOrRequest(obj.ENRApiCall)+"Async",
+        "ApiCall": stripAsyncOrRequest(obj.ENRApiCall) + "Async",
         "RequestParameters": obj.ENRRequestParameters,
         "RequiredPermissionConfig": {
             "RequiredPermission": obj.ENRRequiredPermission,
-            "EntityType":  obj.ruleTargetType,
+            "EntityType": obj.ruleTargetType,
             "EntitySubType": obj.ENRSubType
         },
         "RequestInfo": "Amazon." + obj.AssemblyName + ".Model." + stripAsyncOrRequest(obj.ENRApiCall) + "Request, AWSSDK." + obj.AssemblyName,
@@ -100,7 +87,6 @@ function handleSubmit() {
         "ResponsePropertyToUse": obj.ENRResponsePropertyToUse,
         "PropertiesToRemoveFromExternalObject": formatArray(obj.ENRPropertiesToRemoveFromExternalObject)
     } : undefined;
-
 
 
     let json = {
@@ -132,8 +118,7 @@ function handleSubmit() {
             "RequestInfo": "Amazon." + obj.AssemblyName + ".Model." + stripAsyncOrRequest(obj.ApiCall) + "Request, AWSSDK." + obj.AssemblyName,
             "ApiCall": stripAsyncOrRequest(obj.ApiCall) + "Async",
             "RequiredPermissionConfig": {
-                "RequiredPermission": obj.RequiredPermission,
-                "EntityType": obj.ruleTargetType
+                "RequiredPermission": obj.RequiredPermission, "EntityType": obj.ruleTargetType
             },
             "RequestParameters": RequestParameters,
             "PaginationMarker": obj.PaginationMarker,
@@ -145,6 +130,10 @@ function handleSubmit() {
     document.getElementById("result").innerText = json;
     return json;
 }
+
+const stripAsyncOrRequest = str => str.replace(/(async|request)$/i, '');
+const stripAwsOrEntity = str => str.replace(/^(Aws)/i, '').replace(/(Entity)$/i, '');
+const stripJson = str => str.replace(/(.json)$/i, '');
 
 function saveTextAsFile() {
     var textToWrite = handleSubmit()
@@ -173,12 +162,10 @@ function saveTextAsFile() {
 
 function isEmpty(value) {
     if (Array.isArray(value)) {
-        if (value.length === 0 || value.every(item => item === '') ) return true
+        if (value.length === 0 || value.every(item => item === '')) return true
     }
-    return (value === undefined || value === "" || value == null ||
-        (typeof value === 'object' && Object.keys(value).length === 0));
+    return (value === undefined || value === "" || value == null || (typeof value === 'object' && Object.keys(value).length === 0));
 }
-
 
 function replacer(key, value) {
     if (isEmpty(value)) {
@@ -199,20 +186,72 @@ function formatArray(input) {
     return [input];
 }
 
-
-
-function mutuallyExclusive(id1,id2) {
+function mutuallyExclusive(id1, id2) {
     let input1 = document.getElementById(id1);
     let input2 = document.getElementById(id2);
 
-    input1.addEventListener('input', function() {
+    input1.addEventListener('input', function () {
         input2.disabled = !!input1.value;
     });
 
-    input2.addEventListener('input', function() {
+    input2.addEventListener('input', function () {
         input1.disabled = !!input2.value;
     });
 }
 
-mutuallyExclusive("ResponsePropertyToUse","PropertiesToRemoveFromExternalObject")
-mutuallyExclusive("ENRResponsePropertyToUse","ENRPropertiesToRemoveFromExternalObject")
+mutuallyExclusive("ResponsePropertyToUse", "PropertiesToRemoveFromExternalObject")
+mutuallyExclusive("ENRResponsePropertyToUse", "ENRPropertiesToRemoveFromExternalObject")
+
+function getContinueWith() {
+    return Array.from(document.querySelectorAll('#ContinueWithList li')).map(function (li) {
+        return stripJson(li.textContent);
+    });
+}
+
+function getRequestParameters() {
+    return Array.from(document.querySelectorAll('#RequestParametersList li')).map(function (li) {
+        let val = li.textContent.replace(/[{}"]/g, '').split(':')
+        return {
+            "ParameterName": val[0], "ParameterValue": val[1]
+        };
+    });
+}
+
+function saveToLocalStorage() {
+    for (let i = 0; i < form.elements.length; i++) {
+        let key = form.elements[i].id;
+        let value = form.elements[i].value;
+        localStorage.setItem(key, value);
+    }
+    localStorage.setItem("IsRulesEngineBaseEntity", document.getElementById("IsRulesEngineBaseEntity").checked)
+    localStorage.setItem("ShouldEnrichBaseEntity", document.getElementById("ShouldEnrichBaseEntity").checked)
+    localStorage.setItem("IsCronTriggered", document.getElementById("IsCronTriggered").checked)
+    localStorage.setItem("ShouldIndexEntity", document.getElementById("ShouldIndexEntity").checked)
+    localStorage.setItem("IsRegionLess", document.getElementById("IsRegionLess").checked)
+    localStorage.setItem("IsExternalIdGenerated", document.getElementById("IsExternalIdGenerated").checked)
+    localStorage.setItem("ShouldEnrichBaseEntity", document.getElementById("ShouldEnrichBaseEntity").checked)
+}
+
+function loadFromLocalStorage() {
+    for (let i = 0; i < form.elements.length; i++) {
+        let key = form.elements[i].id;
+        form.elements[i].value = localStorage.getItem(key);
+    }
+    const forceClick = (id) => {
+        let elem = document.getElementById(id);
+        let value = localStorage.getItem(id) === "true"
+        let state = elem.checked
+        if (value !== state) {
+            elem.click()
+        }
+    }
+    forceClick("IsRulesEngineBaseEntity")
+    forceClick("ShouldEnrichBaseEntity")
+    forceClick("IsCronTriggered")
+    forceClick("ShouldIndexEntity")
+    forceClick("IsRegionLess")
+    forceClick("IsExternalIdGenerated")
+    forceClick("ShouldEnrichBaseEntity")
+    handleSubmit()
+
+}
